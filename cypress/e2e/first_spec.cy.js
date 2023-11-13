1166571428dc64e731e6a79ea4991b43f5e715b7
Teddy Williams <TedsterDev@gmail.com>
2023-11-13 14:16:00 -0800

    feat(github): changes to nike.com

--- cypress/e2e/first_spec.cy.js
+++ cypress/e2e/first_spec.cy.js
@@ -1,7 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://offerup.com')
-    cy.get('.jss157').type('Hello World\n')
-    cy.get('.jss3484').click
+    cy.visit('https://nike.com')
   })
 })
