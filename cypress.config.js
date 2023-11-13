a61413737f0790616d34d26ffe54c23ddb9a7eec
Teddy Williams <TedsterDev@gmail.com>
2023-11-13 12:46:48 -0800

    initial comment - cypress auto gen code

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ...rest of the Cypress project config
+  projectId: "wvfse5",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
